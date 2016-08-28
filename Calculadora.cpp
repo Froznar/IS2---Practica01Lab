5e5432cc03b4534b547fed11e23cae4c4e16c86e
Francisco Marin <Francisco Marin>
2016-08-27 23:32:31 -0500

    Funcion realizada

--- Calculadora.cpp
+++ Calculadora.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 using namespace std;
 
+
 //operacion x(int a, int b)
 //{ colocar los "cin>>" requeridos  }
+void sustraccion()
+{
+    int a,b;
+    cout<<"ingrese los valores"<<endl;
+    cin>>a;
+    cin>>b;
+    cout<<"resp: "<<a-b<<endl;
+}
+
 int main(){
 
     int caso;
@@ -11,13 +21,14 @@ int main(){
 
         cout<<endl;
         //cout<<"1: operacion x"<<endl;
-
+        cout<<"2: Sustraccion"<<endl;
         cout<<"0: Salir"<<endl;
 
         cout<<"Selecione un caso"<<endl;
+        cout<<"------------------"<<endl;
         cin>>caso;
         //if(caso==0){breack;}
-
+        if(caso==2){sustraccion();}
 
 
 
