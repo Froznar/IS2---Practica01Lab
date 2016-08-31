612eb046c32e3db38813747f9d61f5771006619d
Rapange <h_ero@hotmail.es>
2016-08-31 12:19:29 -0500

    Modifiqué la suma

--- Calculadora.cpp
+++ Calculadora.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int sumar(int a, int b)
+int sumar_recursiva(int a, int b)
 {
     if(b==0) return a;
     if(b > 0) return 1 + sumar(a,b-1);
     return -1 + sumar(a,b+1);
+}
+
+int sumar()
+{
+    cout<<"Ingrese los valores:"<<endl;
+    cin>>a>>b;
+    return sumar_recursiva(a,b);
 }
 
 int main(){
@@ -15,18 +22,13 @@ int main(){
     do{
 
         cout<<endl;
+	cout<<"1: Sumar"<<endl;
         cout<<"0: Salir"<<endl;
-	cuut<<"1: Operacion Sumar"<<endl
 	
-        cout<<"Selecione un caso"<<endl;
+        cout<<"Seleccione un caso"<<endl;
         cin>>caso;
         //if(caso==0){breack;}
-	if(caso==1)
-	{
-	    cout<<"Ingrese dos numeros:"<<endl;
-	    cin>>a>>b;
-	    cout<<sumar(a,b)<<endl;
-	}
+	if(caso==1) cout<<"La suma es: "<<sumar(a,b)<<endl;
 
 
     }while(caso!=0);
