a25ba5989d4d304eab0479a1bc16ae9b09b88acc
RenzoAlessandro <renzo.sucari@ucsp.edu.pe>
2016-08-31 17:41:48 -0500

    Update Calculadora.cpp

--- Calculadora.cpp
+++ Calculadora.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 using namespace std;
 
-//operacion x(int a, int b)
-//{ colocar los "cin>>" requeridos  }
+///Esta exponenciacion es la mas simple con numeros cortos,
+///la funcion no es complicada es la mas basica.
+int Exponenciacion(int base, int potencia){
+    int respuesta=1;
+    for (int i=1 ; i<=potencia; i++)
+    {
+        respuesta=respuesta*base;
+    }
+    return respuesta;
+}
+
 int main(){
 
     int caso;
@@ -10,17 +19,19 @@ int main(){
     do{
 
         cout<<endl;
-        //cout<<"1: operacion x"<<endl;
+        cout<<"7: Exponenciacion"<<endl;
 
         cout<<"0: Salir"<<endl;
 
         cout<<"Selecione un caso"<<endl;
         cin>>caso;
-        //if(caso==0){breack;}
-
-
-
-
+        if(caso==7)
+        {
+            int base, potencia;
+            cout << "  Ingresar la base ";cin >> base;
+            cout << "  Ingresar la potencia ";cin >> potencia;
+            cout << " El resultado es: " <<Exponenciacion(base,potencia)<<endl;
+        }
 
 
     }while(caso!=0);
