f39d603a2a27c611bd941c0da964b7ef049ef392
Rodrigo Gutierrez <rodrigo.gutierrez.k@gmail.com>
2016-08-30 22:10:09 -0500

    Agregada la funcionalidad multiplicacion"

--- Calculadora.cpp
+++ Calculadora.cpp
@@ -3,6 +3,15 @@ using namespace std;
 
 //operacion x(int a, int b)
 //{ colocar los "cin>>" requeridos  }
+void multiplicacion(){
+    int a, b;
+    cout << "Ingrese el primer factor: ";
+    cin >> a;
+    cout << "Ingrese el segundo factor: ";
+    cin >> b;
+    cout << "Respuesta: " << a*b << endl;
+}
+
 int main(){
 
     int caso;
@@ -11,12 +20,16 @@ int main(){
 
         cout<<endl;
         //cout<<"1: operacion x"<<endl;
+        cout << "3: Multiplicacion." << endl;
 
         cout<<"0: Salir"<<endl;
 
         cout<<"Selecione un caso"<<endl;
         cin>>caso;
         //if(caso==0){breack;}
+        
+        if(caso == 3)
+            multiplicacion();
 
 
 
