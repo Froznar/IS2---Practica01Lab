453d4ccae11bea4add4b8b15ebbb7062c37d4fdb
Rapange <h_ero@hotmail.es>
2016-08-31 12:06:42 -0500

    Agregué suma.

--- Calculadora.cpp
+++ Calculadora.cpp
@@ -1,26 +1,32 @@
 #include <iostream>
 using namespace std;
 
-//operacion x(int a, int b)
-//{ colocar los "cin>>" requeridos  }
+int sumar(int a, int b)
+{
+    if(b==0) return a;
+    if(b > 0) return 1 + sumar(a,b-1);
+    return -1 + sumar(a,b+1);
+}
+
 int main(){
 
-    int caso;
+    int caso,a,b;
 
     do{
 
         cout<<endl;
-        //cout<<"1: operacion x"<<endl;
-
         cout<<"0: Salir"<<endl;
-
+	cuut<<"1: Operacion Sumar"<<endl
+	
         cout<<"Selecione un caso"<<endl;
         cin>>caso;
         //if(caso==0){breack;}
-
-
-
-
+	if(caso==1)
+	{
+	    cout<<"Ingrese dos numeros:"<<endl;
+	    cin>>a>>b;
+	    cout<<sumar(a,b)<<endl;
+	}
 
 
     }while(caso!=0);
